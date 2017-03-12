da9b7075162c21432ead31a839123cb49f95cbf5
Solomon Bothwell <ssbothwell@gmail.com>
2017-03-11 16:50:30 -0800

    added breadth first search and bfs connected components

---
+++ graphs/bfsConnectedComponents.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from collections import deque
+
+def bfSearch(gDict, startingNode, exploredDict=None):
+    if exploredDict == None:
+        exploredDict = { key: (True if key == startingNode else False)  for key in gDict }
+    else:
+        exploredDict[startingNode] = True
+
+    distDict = {startingNode: 0}
+    queue = deque([startingNode])
+
+    while len(queue) != 0:
+        n = queue.popleft()
+        for m in graphDict[n]:
+            if exploredDict[m] == False:
+                distDict[m] = distDict[n] + 1
+                exploredDict[m] = True
+                queue.extend(m)
+    return distDict
+
+def connectedComponents(gDict):
+    exploredDict = { key: False for key in gDict }
+    connectedDict = []
+    for n in gDict:
+        if exploredDict[n] == False:
+            connectedDict.append(bfSearch(gDict, n, exploredDict))
+    return connectedDict
+
+graphDict = {
+    '1': ['3','5'],
+    '2': ['4'],
+    '3': ['1','5'],
+    '4': ['2'],
+    '5': ['1','3', '7', '9'],
+    '6': ['8','10'],
+    '7': ['5'],
+    '8': ['6','10'],
+    '9': ['5'],
+    '10': ['6','8']
+}
+
+print connectedComponents(graphDict)
---
+++ graphs/breadthFirstSearch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from collections import deque
+
+def bfSearch(gDict, startingNode):
+    exploredDict = { key: (True if key == startingNode else False)  for key in gDict.keys() }
+    distDict = {startingNode: 0}
+    queue = deque(startingNode)
+
+    while len(queue) != 0:
+        n = queue.popleft()
+        for m in graphDict[n]:
+            if exploredDict[m] == False:
+                distDict[m] = distDict[n] + 1
+                exploredDict[m] = True
+                queue.extend(m)
+
+    return exploredDict, distDict
+
+graphDict = {
+    's': ['a','b'],
+    'a': ['s','c'],
+    'b': ['s','c', 'd'],
+    'c': ['a','b','d','o'],
+    'd': ['b','c','o'],
+    'o': ['c','d']
+}
+
+print bfSearch(graphDict, 's')
