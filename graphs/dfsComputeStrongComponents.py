b8ae46b694fb097a3bda2278769fafaf42045e0a
Solomon Bothwell <ssbothwell@gmail.com>
2017-03-16 19:23:41 -0700

    fixed bug in graph reverse function

--- graphs/dfsComputeStrongComponents.py
+++ graphs/dfsComputeStrongComponents.py
@@ -3,7 +3,7 @@
 
 def reverseGraph(graph):
     """ Reverse the key/value pairs in linked list graph """
-    rGraph = {}
+    rGraph = { el: [] for el in range(1, 1+max(max(graph.keys()),max(graph.values()[0])))}
     for key in graph:
         for val in graph[key]:
             if val in rGraph:
@@ -13,54 +13,50 @@ def reverseGraph(graph):
     return rGraph
 
 
-def visit(gDict, startingNode, t):
+def visit(gDict, startingNode, exploredDict, t):
     """ Visit each node using DFS generate finishing time list """
-    global exploredDict
 
     exploredDict[startingNode] = True
     for n in gDict[startingNode]:
         if exploredDict[n] == False:
-            visit(gDict, n, t)
+            visit(gDict, n, exploredDict, t)
     t.append(startingNode)
 
 
-def assign(gdict, startingnode, leader):
+def assign(gdict, startingnode, exploredDict, leader, sComponent):
     """ Second pass DFS group nodes with leaders """
-    global exploredDict
-    global strongComponents
 
     exploredDict[startingnode] = True
     for n in gdict[startingnode]:
         if exploredDict[n] == False:
-            strongComponents[leader].append(n)
-            assign(gdict, n, leader)
+            sComponent.append(n)
+            assign(gdict, n, exploredDict, leader, sComponent)
 
 
 def dfsloop(gDict):
-    global exploredDict
-    global strongComponents
     # Pass Two Order
     t = []
     # Current `leader` Vertex
     s = -1
-    strongComponents = {}
+    strongComponents = []
     exploredDict = { key: False for key in gDict.keys() }
-
     # First Pass
-    for i in range(len(gDict), 0, -1):
+    for i in reversed(gDict.keys()):
         if exploredDict[i] == False:
             s = i
-            visit(gDict, i, t)
+            visit(gDict, i, exploredDict, t)
 
     # Second Pass
-    rGraph = reverseGraph(graph)
-    exploredDict = { key: False for key in gDict.keys() }
+    rGraph = reverseGraph(gDict)
+    exploredDict = { key: False for key in rGraph.keys() }
+    #t = filter(lambda x: x != 5, t)
     # Uses finishing times as order
-    for i in range(len(t)-1, -1, -1):
-        s = t[i]
-        if exploredDict[t[i]] == False:
-            strongComponents[s] = []
-            assign(rGraph, t[i], s)
+    for i in reversed(t):
+        s = i
+        if exploredDict[i] == False:
+            sComponent = [s]
+            assign(rGraph, i, exploredDict, s, sComponent)
+            strongComponents.append(sComponent)
     return strongComponents
 
 
@@ -76,5 +72,17 @@ graph = {
     9: [6]
 }
 
-print dfsloop(graph)
+# Load data and process into a graph dict
+file = open('scc2.txt', 'r')
+data = map(lambda x: [int(x[0]), int(x[1])], map(lambda x: x.rstrip().split(' '), file))
+
+file = open('scc2.txt', 'r')
+rData = map(lambda x: [int(x[1]), int(x[0])], map(lambda x: x.rstrip().split(' '), file))
+
+#graphDict = { el[0]: [] for el in data}
+graphDict = { el: [] for el in range(1, 1+max(map(lambda x: max(x), data)))}
+#rGraphDict = { el: [] for el in range(1, 1+max(map(lambda x: max(x), rData)))}
+map(lambda el: graphDict[el[0]].append(el[1]), data)
+#map(lambda el: rGraphDict[el[0]].append(el[1]), rData)
+print sorted(map(lambda x: len(x), dfsloop(graphDict)), reverse=True)
 
