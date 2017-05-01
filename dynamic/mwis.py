35ef73df528c7a2dc41bee27394816c77219aba0
Solomon Bothwell <ssbothwell@gmail.com>
2017-04-30 22:01:07 -0700

    expanded comments for mwis

--- dynamic/mwis.py
+++ dynamic/mwis.py
@@ -11,7 +11,7 @@ For example:
 V := [1] - [4] - [5] - [4]
 Solution == [4,4] == 8
 
-For any optimal solutionto MWIS, one of the following two
+Given an optimal solution to MWIS, one of the following two
 statements is true:
     1. The last element is not part of the optimal set. This
         also means that the last element is not part of any
@@ -21,7 +21,42 @@ statements is true:
         optimal set or any subgraph V'' of V formed by removing
         the last two elements of V.
 
+If we somehow knew which one of these statements was true for
+a given graph. We could then recursively solve the problem.
+However, the recursive solution takes exponential time. This is
+because we only throw out either 1 or 2 vertices at each recursive
+call. So we are branching twice for every 1 or 2 vertices.
+
+However, across all these recursive calls, there are only N
+unique sub problems (where N is the size of the origina graph).
+These subproblems are all the subgraphs with either 1 or 2 vertex
+removed. The exponential runtime of the recursive algorithm is a
+result of solving these subproblems over and over again.
+
+If we cache the solutions for each of these subproblems as we
+generate them, we can drastically reduce the run time. This is
+called 'memoization'. Working from the smallest subgraph (of size 1
+and weight G[0]) we can check which of the two statements results
+in the max size subgraph and record that in A[i].
+
+Pseudocode:
+G = Problem Graph
+A = array of max weights of subgraphs
+for i in G:
+    A[i] = max(A[i-1], A[i-2]+G[i])
+return A[-1]
 """
+
+def recursive_mwis(graph):
+    # naive recursive version
+    if len(graph) == 1:
+        return graph[0]
+    elif len(graph) < 1:
+        return 0
+    else:
+        s1 = recursive_mwis(graph[:-1])
+        s2 = graph[-1] + recursive_mwis(graph[:-2])
+        return max(s1,s2)
 
 def mwis(graph):
     V = graph[:]
@@ -29,18 +64,14 @@ def mwis(graph):
         if i == 0:
             V[i] = V[i]
         elif i == 1:
-            if V[i-1] > el:
-                V[i] = V[i-1]
-            else:
-                    V[i] = V[i]
+            V[i] = max(V[i-1], V[i])
         else:
-            if V[i-1] > V[i-2] + V[i]:
-                V[i] = V[i-1]
-            else:
-                V[i] = V[i-2] + V[i]
+            V[i] = max(V[i-1], V[i-2] + V[i])
     return V
 
-def reconstruct(path, orig):
+def reconstruct(path):
+    """ Returns the indices of the vertices of the MWIS """
+    orig = range(1, len(path)+1)
     result = []
     i = len(path)-1
     while i >= 1:
@@ -62,10 +93,11 @@ def load_path(filename):
 
 
 if __name__ == '__main__':
-    #a = [1,4,5,4] #[4,4]
+    a = [1,4,5,4] #[4,4]
     #a = [280,618,762,908,409,34,59,277,246,779] #[2,4,6,8,10]
     #a = [460,250,730,63,379,638,122,435,705,84] #[1,3,6,9]
-    a = load_path('data_mwis.txt')
-    j = range(1, len(a)+1)
-    result = reconstruct(mwis(a), j)
-    print([ x for x in [1,2,3,4,17,117,517,997] if x in result])
+    #a = load_path('data_mwis.txt')
+    result = reconstruct(mwis(a))
+    print(result)
+    # This list comprehension is specific to coursera assignment
+    #print([ x for x in [1,2,3,4,17,117,517,997] if x in result])
